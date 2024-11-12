f0f9353e7de5a3610468b1630ea27357885cf58d
MadhusreeJ <68596009+MadhusreeJ@users.noreply.github.com>
2024-11-13 00:55:10 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -25,19 +25,16 @@ router.post('/register-admin', async function(req, res, next) {
     const { mail, password } = req.body;
 
     try {
-        // Find the user by email
         const admin = await Admin.findOne({ mail });
         if (!admin) {
             return res.status(400).json({ message: 'No admin found' });
         }
 
-        // Compare the provided password with the stored hashed password
         const passwordCheck = await bcrypt.compare(password, admin.password);
         if (!passwordCheck) {
             return res.status(400).json({ message: 'Invalid email or password' });
         }
 
-        // Successful login
         res.status(200).json({ message: 'Login successful'});
     } catch (error) {
         res.status(500).json({ message: 'Server error', error });
